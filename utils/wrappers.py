210fcfee484e910550e271d6e457b9cbc113afa5
benja263 <benjafuhrer@gmail.com>
2024-09-15 19:25:01 +0300

    fixing mistake in wrapper

--- utils/wrappers.py
+++ utils/wrappers.py
@@ -150,7 +150,7 @@ class NeuroSymbolicAtariWrapper(ObservationWrapper):
         super().__init__(env)
         flattened_shape = env.observation_space.shape[0]
         if len(env.observation_space.shape) > 1:
-            flattened_shape = env.observation_space.shape[1]*env.observation_space.shape[2] + env.observation_space.shape[1] - 1 + env.observation_space.shape[1] - 2
+            flattened_shape = env.observation_space.shape[1]*env.observation_space.shape[2] + env.observation_space.shape[1] - 1
         env.observation_space = gym.spaces.Box(low=0, high=255, shape=(flattened_shape, ), dtype=np.float32)
         
     def observation(self, observation):
@@ -164,14 +164,13 @@ class NeuroSymbolicAtariWrapper(ObservationWrapper):
         frame_prev_t = observation[-2][:, :2]
         delta_frame = frame_t - frame_prev_t
         player_position = frame_t[0, :2]
-        enemy_position = frame_t[1, :2]
+        # enemy_position = frame_t[1, :2]
         other_positions = frame_t[1:, :2]
         
         # Vectorized L2 norm (Euclidean distance)
         distances = np.linalg.norm(other_positions - player_position, axis=1)
-        enemy_distances = np.linalg.norm(frame_t[2:, :2] - enemy_position, axis=1)
         pos_and_speed = np.concatenate([frame_t, delta_frame], axis=-1).flatten()
-        return np.concatenate([pos_and_speed, distances, enemy_distances])
+        return np.concatenate([pos_and_speed, distances])
         
     def reset(self,  *, seed: int = None, options: dict[str, Any] | None = None) -> tuple[ObsType, dict[str, Any]]:
         observation, info = self.env.reset(seed=seed)
