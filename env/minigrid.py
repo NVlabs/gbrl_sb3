5776a48c19cdea94f3308a61750d50918269f057
benja263 <benjafuhrer@gmail.com>
2024-11-28 12:47:36 +0200

    fixed minigrid

--- env/minigrid.py
+++ env/minigrid.py
@@ -17,6 +17,7 @@ from minigrid.core.mission import MissionSpace
 from gymnasium.envs.registration import register
 
 
+import numpy as np
 class SequentialPutNearEnv(MiniGridEnv):
     def __init__(self, size=6, numObjs=2, max_steps: int | None = None, **kwargs):
         self.size = size
@@ -133,8 +134,71 @@ class SequentialPutNearEnv(MiniGridEnv):
     def step(self, action):
         preCarrying = self.carrying
 
-        obs, reward, terminated, truncated, info = super().step(action)
         reward = 0
+        terminated = False
+        truncated = False
+        info = {}
+
+        # Get the position in front of the agent
+        fwd_pos = self.front_pos
+
+        # Get the contents of the cell in front of the agent
+        fwd_cell = self.grid.get(*fwd_pos)
+
+        # Rotate left
+        if action == self.actions.left:
+            self.agent_dir -= 1
+            if self.agent_dir < 0:
+                self.agent_dir += 4
+
+        # Rotate right
+        elif action == self.actions.right:
+            self.agent_dir = (self.agent_dir + 1) % 4
+
+        # Move forward
+        elif action == self.actions.forward:
+            if fwd_cell is None or fwd_cell.can_overlap():
+                self.agent_pos = tuple(fwd_pos)
+            if fwd_cell is not None and fwd_cell.type == "goal" and self.completed_first_task and preCarrying:
+                terminated = True
+                reward = self._reward() + self.accumulated_reward
+            if fwd_cell is not None and fwd_cell.type == "lava":
+                terminated = True
+
+        # Pick up an object
+        elif action == self.actions.pickup:
+            if fwd_cell and fwd_cell.can_pickup():
+                if self.carrying is None:
+                    self.carrying = fwd_cell
+                    self.carrying.cur_pos = np.array([-1, -1])
+                    self.grid.set(fwd_pos[0], fwd_pos[1], None)
+
+        # Drop an object
+        elif action == self.actions.drop:
+            if not fwd_cell and self.carrying:
+                self.grid.set(fwd_pos[0], fwd_pos[1], self.carrying)
+                self.carrying.cur_pos = fwd_pos
+                self.carrying = None
+
+        # Toggle/activate an object
+        elif action == self.actions.toggle:
+            if fwd_cell:
+                fwd_cell.toggle(self, fwd_pos)
+
+        # Done action (not used by default)
+        elif action == self.actions.done:
+            pass
+
+        else:
+            raise ValueError(f"Unknown action: {action}")
+
+        if self.step_count >= self.max_steps:
+            truncated = True
+
+        if self.render_mode == "human":
+            self.render()
+
+        obs = self.gen_obs()
 
         u, v = self.dir_vec
         ox, oy = (self.agent_pos[0] + u, self.agent_pos[1] + v)
@@ -151,26 +215,22 @@ class SequentialPutNearEnv(MiniGridEnv):
         # If successfully dropping an object near the target
         if action == self.actions.drop and preCarrying:
             if self.grid.get(ox, oy) is preCarrying:
-                if abs(ox - tx) <= 1 and abs(oy - ty) <= 1:
+                if abs(ox - tx) <= 1 and abs(oy - ty) <= 1 and not self.completed_first_task:
                     reward = self._reward()
-                    if self.completed_first_task:
-                        terminated = True
-                        reward += self.accumulated_reward
-                    else:
-                        self.accumulated_reward = reward
-                        self.move_type = self.target_type
-                        self.moveColor = self.target_color
-                        self.target_color = "green"
-                        self.target_type = "goal"
-                        self.target_pos = self.goal_pos
+                    self.accumulated_reward = reward
+                    self.move_type = self.target_type
+                    self.moveColor = self.target_color
+                    self.target_color = "green"
+                    self.target_type = "goal"
+                    self.target_pos = self.goal_pos
 
-                        self.mission = "put the {} {} at the {} {}".format(
-                                                                            self.moveColor,
-                                                                            self.move_type,
-                                                                            self.target_color,
-                                                                            self.target_type,
-                                                                            )
-                        self.completed_first_task = True
+                    self.mission = "put the {} {} at the {} {}".format(
+                                                                        self.moveColor,
+                                                                        self.move_type,
+                                                                        self.target_color,
+                                                                        self.target_type,
+                                                                        )
+                    self.completed_first_task = True
         reward = min(reward, self.reward_range[1])
         return obs, reward, terminated, truncated, info
 
