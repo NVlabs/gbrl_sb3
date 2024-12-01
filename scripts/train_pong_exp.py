d1ea4343d346b1d8384af33f80780e3bbbb23047
benja263 <benjafuhrer@gmail.com>
2024-12-01 22:10:25 +0200

    fixed mistake

--- scripts/train_pong_exp.py
+++ scripts/train_pong_exp.py
@@ -83,7 +83,7 @@ if __name__ == '__main__':
         eval_wrapper_kwargs['training'] = False 
         eval_wrapper_kwargs['norm_reward'] = False 
     for range_type, ns_kwargs in [('full', {'min_value': 0, 'max_value': 255}), ('lower', {'min_value': 0, 'max_value': 128}), ('upper', {'min_value': 128, 'max_value': 255})]:
-        eval_env = make_ram_atari_env(args.env_name, n_envs=1, env_kwargs=env_kwargs, wrapper_kwargs=args.atari_wrapper_kwargs, vec_env_cls=vec_env_cls, neurosymbolic_kwargs=ns_kwargs)
+        eval_env = make_ram_atari_env(args.env_name, n_envs=1, env_kwargs=env_kwargs, wrapper_kwargs=args.atari_wrapper_kwargs, vec_env_cls=vec_env_cls, vec_env_kwargs=vec_env_kwargs, neurosymbolic_kwargs=ns_kwargs)
         if args.wrapper == 'normalize':
             eval_env = VecNormalize(eval_env, **eval_wrapper_kwargs)
         callback_list.append(MultiEvalCallback(
