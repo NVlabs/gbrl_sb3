588a7cf3c8711beca341eeaff2dbe49d53d31d39
benja263 <benjafuhrer@gmail.com>
2024-12-09 18:18:13 +0200

    added spuriousfetch

--- env/minigrid.py
+++ env/minigrid.py
@@ -298,6 +298,104 @@ class DistanceFetchEnv(MiniGridEnv):
         obs['distances'] = self.distances
         return obs, info
 
+class SpuriousFetchEnv(MiniGridEnv):
+    def __init__(self, size=8, numObjs=3, max_steps: int | None = None, train: bool = True, test_box_idx: int = None, **kwargs):
+        self.numObjs = 3
+        self.size = 8
+        self.obj_types = ["ball"]
+        self.colors = {
+            "red": np.array([255, 0, 0]),
+            "green": np.array([0, 255, 0]),
+            "blue": np.array([0, 0, 255]),
+        }
+        if test_box_idx is not None:
+            assert test_box_idx >= 0 and test_box_idx < 3
+        self.test_box_idx = test_box_idx
+        self.color_names = sorted(list(self.colors.keys()))
+        self.train = train
+        MISSION_SYNTAX = [
+            "get a"
+        ]
+        mission_space = MissionSpace(
+            mission_func=self._gen_mission,
+            ordered_placeholders=[MISSION_SYNTAX, self.color_names, self.obj_types],
+        )
+
+        if max_steps is None:
+            max_steps = 5 * self.size**2
+
+        super().__init__(
+            mission_space=mission_space,
+            width=self.size,
+            height=self.size,
+            # Set this to True for maximum speed
+            see_through_walls=True,
+            max_steps=max_steps,
+            **kwargs,
+        )
+
+    def _rand_color(self):
+        """Sample a color with custom probabilities."""
+        return np.random.choice(self.color_names)
+    
+    def _rand_obj(self):
+        """Sample a color with custom probabilities."""
+        return np.random.choice([0, 1, 2])
+    @staticmethod
+    def _gen_mission(syntax: str, color: str, obj_type: str):
+        return f"{syntax} {color} {obj_type}"
+
+    def _gen_grid(self, width, height):
+        self.grid = Grid(width, height)
+
+        # Generate the surrounding walls
+        self.grid.horz_wall(0, 0)
+        self.grid.horz_wall(0, height - 1)
+        self.grid.vert_wall(0, 0)
+        self.grid.vert_wall(width - 1, 0)
+        # Place a goal square in the bottom-right corner
+        if self.train:
+            self.put_obj(Box('red'), width - 2, height - 2)
+
+        objs = []
+        obs_red = Ball('red')
+        obs_green = Ball('green')
+        obs_blue = Ball('blue')
+
+        objs = [obs_red, obs_green, obs_blue]
+        self.place_obj(obs_red)
+        self.place_obj(obs_green)
+        self.place_obj(obs_blue)
+        #     objs.append(obj)
+        # Randomize the player start position and orientation
+        self.place_agent()
+        # Choose a random object to be picked up
+        if self.test_box_idx is None:
+            target = objs[self._rand_obj()]
+        else:
+            target = objs[self.test_box_idx]
+        self.targetType = target.type
+        self.targetColor = target.color
+        descStr = f"{self.targetColor} {self.targetType}"
+        # Generate the mission string
+        self.mission = "get a %s" % descStr
+        assert hasattr(self, "mission")
+
+    def step(self, action):
+        obs, reward, terminated, truncated, info = super().step(action)
+        if self.carrying:
+            if (
+                self.carrying.color == self.targetColor
+                and self.carrying.type == self.targetType
+            ):
+                reward = self._reward()
+                terminated = True
+            else:
+                reward = 0
+                terminated = True
+
+        return obs, reward, terminated, truncated, info
+
 
 
 def register_minigrid_tests():
@@ -354,4 +452,9 @@ def register_minigrid_tests():
         id="MiniGrid-DistanceFetch-8x8-N3-rank-v3",
         entry_point="env.minigrid:DistanceFetchEnv",
         kwargs={"size": 8, "numObjs": 3, "distances_type": "rank", "mission_probs": [1, 0, 0, 1]},
+    )
+    register(
+        id="MiniGrid-SpuriousFetch-8x8-N3-v0",
+        entry_point="env.minigrid:SpuriousFetchEnv",
+        kwargs={"size": 8, "numObjs": 3},
     )
